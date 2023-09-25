b79fb89b60f08290d4698f0569f33467aab925b0
Vasyl Melnyk <vasyl.melnyk@intive.com>
2023-09-25 14:38:40 +0300

    Updated versions and min SDK to 21.

--- plugins/src/main/java/ProjectConfig.kt
+++ plugins/src/main/java/ProjectConfig.kt
@@ -5,7 +5,7 @@ import org.gradle.jvm.toolchain.JavaLanguageVersion
 
 object ProjectConfig {
     const val versionCode = 1
-    const val versionName = "1.0.2"
+    const val versionName = "1.0.3"
 
     const val namespace = "com.gigamole.composeshadowsplus"
     const val group = "com.github.GIGAMOLE"
@@ -14,7 +14,7 @@ object ProjectConfig {
 
     const val compileSdk = 34
     const val targetSdk = 34
-    const val minSdk = 24
+    const val minSdk = 21
 
     val javaCompileVersion = JavaVersion.VERSION_17
     private val javaCompileVersionText = javaCompileVersion.toString()
